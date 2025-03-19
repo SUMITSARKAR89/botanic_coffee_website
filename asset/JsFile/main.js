c93524a96a8944c418f7760a028a6d67ce30020b
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-03-20 02:41:33 +0600

    menu part start

--- asset/JsFile/main.js
+++ asset/JsFile/main.js
@@ -1,3 +1,4 @@
+// ===============================image slider========================
 const sevenBtns = document.querySelectorAll('.bg_btn');
 const sevenImages = document.querySelectorAll('.sevenImg');
 
@@ -24,6 +25,7 @@ sevenBtns.forEach( (act, change) => {
 
 
 
+// ===========================image-slide-btn=============== 
 
 const images = document.querySelectorAll('.img');
 images.forEach( img => {
@@ -31,10 +33,43 @@ images.forEach( img => {
         removeClass (images);
         img.classList.add('show-img')
     })
-})
+});
 
 function removeClass (images){
     images.forEach( img => {
         img.classList.remove('show-img')
     })
-}
+};
+
+
+// ============================FAQ===================
+
+const faqContent = document.querySelectorAll('.faq-content');
+
+faqContent.forEach( faqs => {
+    faqs.addEventListener("click", () => {
+        faqs.classList.toggle("active");
+    })
+});
+
+// ========================menuBar============================
+
+const navigationMenu = document.querySelector(".navigationMenu");
+const openMenu = document.querySelector(".openMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const nav = document.querySelector("nav");
+
+openMenu.addEventListener("click", () => {
+    navigationMenu.style.maxHeight = "70vh";
+    nav.style.display = "flex";
+    openMenu.style.display = "none";
+    closeMenu.style.display = "flex";
+});
+
+closeMenu.addEventListener("click", () => {
+
+    navigationMenu.style.maxHeight = "12vh";
+    nav.style.display = "none";
+    openMenu.style.display = "flex";
+    closeMenu.style.display = "none";
+});
