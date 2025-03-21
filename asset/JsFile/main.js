6d8684f9a179b2fd9fb84aa1ccb42adb9c760a71
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-03-21 22:19:02 +0600

    before responsiveg

--- asset/JsFile/main.js
+++ asset/JsFile/main.js
@@ -144,3 +144,163 @@ localcloseMenu.addEventListener("click", () => {
 
 });
 
+
+
+// ========================backend section cart controler============================
+// 01. --------------cart active and deactive-------------------
+const cartIcon = document.querySelector('#cartIcon');
+const cartBody = document.querySelector('#cart-body');
+const cartClosed = document.querySelector('.cencel-cart');
+
+cartIcon.addEventListener('click', () => {
+    cartBody.classList.add('cart-active');
+  });
+  cartClosed.addEventListener('click', () => {
+    cartBody.classList.remove('cart-active');
+  });
+
+// console.log(cartIcon, cartBody, cartClosed)
+
+// 02. --------------shopping cart buttons active-------------------
+// 02. step01
+const addCartBtns = document.querySelectorAll('.add-cart');
+addCartBtns.forEach( (btn) => {
+  btn.addEventListener('click', event =>{
+    const productBox = event.target.closest('.productBox'); //closest
+    addToCart(productBox);
+  });
+});
+
+// 03. --------------define add cart button -------------------
+// 02. step04
+const cartBodyContent = document.querySelector('.cart-body-content');
+// 02. step02 main function
+const addToCart = productBox =>{
+    const productImgSrc = productBox.querySelector('.good-img img').src;
+    const productName = productBox.querySelector('.headTitle-shop').textContent;
+    const productPrice = productBox.querySelector('.price').textContent;
+
+// 02. step03
+    const newCartBox = document.createElement('div');
+    newCartBox.classList.add('cart-box');
+
+    newCartBox.innerHTML =`
+        <img src="${productImgSrc}" alt="" class="cart-img">
+        <div class="cart-details">
+            <h5 class="cart-details-name">${productName}</h5>
+            <span class="cart-price">${productPrice}</span>
+            <div class="cart-qty">
+                <button id="minus">-</button>
+                <span class="number">1</span>
+                <button id="plus">+</button>
+            </div>
+        </div>
+        <i class="ri-delete-bin-6-line" id="cardRemove"></i> 
+    `;
+
+    //  03. -------------------alert set when selected same item -------------- 
+    const selectedSameItem = cartBodyContent.querySelectorAll('.cart-details-name');
+    for(let item of selectedSameItem){
+      if(item.textContent === productName){
+        alert('This product is already in the CART')
+        return;
+      }
+    }
+
+    // 02. step04
+    cartBodyContent.appendChild(newCartBox);
+
+     //  04. -------------------remove cart items when click trash box -------------- 
+     newCartBox.querySelector('#cardRemove').addEventListener('click', () => {
+        newCartBox.remove();
+    
+        updateCartCountBadge(-1); //call from 07
+        updateTotalPrice(); //call from 06
+    });
+
+
+     //  05. -------------------remove cart items when click trash box -------------- 
+    newCartBox.querySelector('.cart-qty').addEventListener('click', event => {
+    const numberElement = newCartBox.querySelector('.number');
+    const minusBtn = newCartBox.querySelector('#minus');
+  
+    let qty = numberElement.textContent;
+  
+    if (event.target.id === 'minus' && qty > 1) {
+      numberElement.textContent = --qty;
+      if (qty === 1) minusBtn.style.color = '#999'; 
+    } 
+    else if (event.target.id === 'plus') {
+      numberElement.textContent = ++qty;
+      minusBtn.style.color = '#333'; 
+    }
+    updateTotalPrice(); //call from 06
+  });
+
+  updateCartCountBadge(1); //call from 07
+  updateTotalPrice(); //call from 06
+};
+
+
+//  06. -------------------update total price -------------- 
+
+const updateTotalPrice = () => {
+    const totalPrice = document.querySelector('.total-price');
+    const cartBoxes = cartBodyContent.querySelectorAll('.cart-box');
+    let total = 0;
+    cartBoxes.forEach( cartbox => {
+      const priceElement = cartbox.querySelector('.cart-price');
+      const qtyElement = cartbox.querySelector('.number');
+      let price = parseFloat(priceElement.textContent.replace('$', '')) || 0;
+      const qty =parseInt(qtyElement.textContent) || 0;
+  
+      total += price * qty;
+  
+    });
+    totalPrice.textContent = `$${total}`;
+  };
+
+
+  //  07. -------------------counting cart icon  on  -------------- 
+
+let cartCountIndex = 0;
+const updateCartCountBadge = change => {
+  const cartItemCount = document.querySelector('.cart-count');
+
+  cartCountIndex += change;
+  if(cartCountIndex > 0){
+    cartItemCount.style.visibility = 'visible';
+    cartItemCount.textContent = cartCountIndex;
+  }else{
+    cartItemCount.style.visibility = 'hidden';
+    cartItemCount.textContent = '';
+
+  }
+
+};
+
+//  08. -------------------buy button update --------------
+
+
+const buyButton = document.querySelector('.buy_btn');
+buyButton.addEventListener('click', () => {
+  const cartBox = document.querySelectorAll('.cart-box');
+
+  if(cartBox.length === 0) {
+    alert('your cart is empty');
+    return;
+  }
+  cartBox.forEach ( cartBox => cartBox.remove());
+  cartCountIndex = 0;
+  updateCartCountBadge(0); //call from 07
+  updateTotalPrice(); //call from 06
+
+  alert('Thank you for purshase');
+})
+
+
+
+    
+
+
+
